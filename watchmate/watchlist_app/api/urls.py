740bdf9c4da867807d61df0961d9bde2c8bda46d
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2021-12-29 21:33:04 +0900

    ConcreteViewClasse

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
 from watchlist_app.api.views import (ReviewList,
+                                    ReviewCreate,
                                     ReviewDetail, 
                                     WatchListAV, 
                                     WatchDetailAV,
@@ -14,7 +15,8 @@ urlpatterns = [
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name= 'stream-detail'),
 
     path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name='stream-detail'),
-    path('stream/review/<int:pk>', ReviewDetail.as_view(), name= 'review-detail'),
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name='stream-detailreview-create'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 
     path('review/', ReviewList.as_view(), name='review-list'),
     path('review/<int:pk>', ReviewList.as_view(), name='review-list')
