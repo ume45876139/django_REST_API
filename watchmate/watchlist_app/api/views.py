1651c7c72ffa1986271e2b5769d1f417185570d1
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2021-12-31 15:54:30 +0900

    MEdoelViewSet

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -50,18 +50,31 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 #     def post(self, request, *args, **kwargs):
 #         return self.create(request, *args, **kwargs)
 
-class StreamPlatformVS(viewsets.ViewSet):
+class StreamPlatformVS(viewsets.ModelViewSet):
+  queryset = StreamPlatform.objects.all()
+  serializer_class = StreamPlatformSerializer
 
-  def list(self, request):
-    queryset = StreamPlatform.objects.all()
-    serializer = StreamPlatformSerializer(queryset, many=True, context={'request':request})
-    return Response(serializer.data)
+# class StreamPlatformVS(viewsets.ViewSet):
 
-  def retrieve(self, request, pk=None):
-    queryset = StreamPlatform.objects.all()
-    watchlist = get_object_or_404(queryset, pk=pk)
-    serializer = StreamPlatformSerializer(watchlist)
-    return Response(serializer.data)
+#   def list(self, request):
+#     queryset = StreamPlatform.objects.all()
+#     serializer = StreamPlatformSerializer(queryset, many=True, context={'request':request})
+#     return Response(serializer.data)
+
+#   def retrieve(self, request, pk=None):
+#     queryset = StreamPlatform.objects.all()
+#     watchlist = get_object_or_404(queryset, pk=pk)
+#     serializer = StreamPlatformSerializer(watchlist)
+#     return Response(serializer.data)
+
+#   def create(self, request):
+#     serializer = StreamPlatformSerializer(data=request.data)
+#     if serializer.is_valid():
+#       serializer.save()
+#       return Response(serializer.data)
+#     else:
+#       return Response(serializer.errors)
+    
 
 class StreamPlatformAV(APIView):
 
