232d791b660df36b81baf6d918485cd75fe8e7ff
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2022-01-02 15:35:36 +0900

    TempLoginOut

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+
 from rest_framework import status
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
@@ -15,11 +17,20 @@ from watchlist_app.api.serializers import (WatchListSerializer,
 class ReviewCreate(generics.CreateAPIView):
   serializer_class = ReviewSerializer
 
+  def get_queryset(self):
+    return Review.objects.all()
+
   def perform_create(self, serializer):
     pk = self.kwargs.get('pk')
-    movie = WatchList.objects.get(pk=pk)
+    watchlist = WatchList.objects.get(pk=pk)
 
-    serializer.save(watchlist=movie)
+    review_user = self.request.user
+    review = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+
+    if review_queryset.exists():
+      raise ValidationError("You have already reviewed this movie.")
+
+    serializer.save(watchlist=watchlist, review_user=review_user)
 
 class ReviewList(generics.ListCreateAPIView):
   # queryset = Review.objects.all()
