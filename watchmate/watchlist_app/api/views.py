635f9eb1c0df42284cc5d9bd85e8a3a20dfc213c
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2022-01-03 15:55:23 +0900

    CustomPermissio

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -45,7 +45,7 @@ class ReviewCreate(generics.CreateAPIView):
 class ReviewList(generics.ListCreateAPIView):
   queryset = Review.objects.all()
   serializer_class = ReviewSerializer
-  permission_classes = [AdminOrReadOnly]
+  permission_classes = [IsAuthenticated]
 
   def get_queryset(self):
     pk = self.kwargs['pk']
