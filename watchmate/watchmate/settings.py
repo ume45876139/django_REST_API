f6f027cbe4f1f9de5b7ca227cabfa0f3de46d31b
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2022-01-08 22:23:37 +0900

    TokenAuthentication2

--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -123,10 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-        'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
--- watchmate/watchmate/urls.py
+++ watchmate/watchmate/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watchlist_app.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('account/', include('user_app.api.urls')),
 ]
