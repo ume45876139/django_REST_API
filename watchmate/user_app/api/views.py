6795f50294120181b13c30fb28827a804080b551
梅北文仁 <umekitafumihito@umekitafumihitonoMacBook-Pro.local>
2022-01-09 21:09:17 +0900

    TokenAuthentication_auto_return_token

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,13 +1,28 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from user_app.api.serializers import RegistrationSerializer
-
+from user_app import models
 
 @api_view(['POST'])
 def registration_view(request):
   
   if request.method == 'POST':
     serializer = RegistrationSerializer(data=request.data)
+
+    data = {}
+
     if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
+      account = serializer.save()
+
+      data['response'] = "Registration Successfull!"
+      data['username'] = account.username
+      data['email'] = account.email
+
+      token = Token.objects.get(user=account).key
+      data['token'] = token
+       
+    else:
+      data = serializer.errors
+
+    return Response(data)
